e28b93a10882260c3f73d073500049d6dfe73de5
Jacob Field <48722956+Prelimine-JF@users.noreply.github.com>
2020-09-14 22:15:25 -0400

    Copied classes to new solutions
    Have not added references in LitKit1

---
+++ Services.Base/Delegates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    /// <summary>
+    /// Use to create an event that publishes when creating a new Redaction in Redaction.cs > public event RedactionCalledDelegate RedactionCalled;
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    public delegate void RedactionCalledDelegate(object sender, EventArgs args);
+
+    public class RedactionCalledMethods
+    {
+        public static void OnRedactionAdded(object sender, EventArgs e)
+        { 
+            //System.Windows.Forms.MessageBox.Show("Redaction Added"); 
+        }
+    }
+
+    public delegate void LogActionDelegate(object sender, EventArgs args);
+
+    public class LogActionClass
+    {
+        public static void OnActionLogged(object sender, EventArgs e)
+        {
+            // Add code here to log an action somewhere
+        }
+    }
+}
---
+++ Services.RibbonButtons/BlockQuotes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+
+namespace Services.RibbonButtons
+{
+    /// <summary>
+    /// Searches for quotes of over x number of words and transforms them into block quotes
+    /// </summary>
+    public static class BlockQuotes
+    {
+        public static void FindQuotesToBlock(Word.Application _app)
+        {
+            System.Windows.Forms.MessageBox.Show("To Do");
+        }
+    }
+}
