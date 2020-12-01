ee8f9bebe5459753af0aa883eb18a11232742022
Jacob Field <48722956+Prelimine-JF@users.noreply.github.com>
2020-12-01 16:14:21 -0500

    Fixed Quotes not replacing, and added functionality to search all story ranges

--- Services.RibbonButtons/SmartQuotesAndApostrophes.cs
+++ Services.RibbonButtons/SmartQuotesAndApostrophes.cs
@@ -18,25 +18,35 @@ namespace Tools.Simple
         {
             _app.Application.System.Cursor = WdCursorType.wdCursorWait;
 
-            _app.ActiveDocument.Select();
-            var rng = _app.Selection;
-            //rng.InsertAfter(" ");
-            rng.Find.Execute(FindText: " ", ReplaceWith: " "); // Something needs to be replaced first or Word 2019/365 closes automatically (exit condition 0) when Replace: WdReplace.wdReplaceAll runs
+            //_app.ActiveDocument.Select();
+            //var rng = _app.Selection;
 
-            rng.Find.Execute(FindText: "\"", ReplaceWith: "\"", Replace: WdReplace.wdReplaceAll);
-            rng.Find.Execute(FindText: "\'", ReplaceWith: "\'", Replace: WdReplace.wdReplaceAll);
+            _app.Selection.Find.Execute(FindText: " ", ReplaceWith: " ", Replace: WdReplace.wdReplaceOne); // Something needs to be replaced first or Word 2019/365 closes automatically (exit condition 0) when Replace: WdReplace.wdReplaceAll runs
+
+            foreach (Range story in _app.ActiveDocument.StoryRanges)
+            {
+                Replace(story, "\'");
+                Replace(story, "\"");
+            }
 
             _app.Application.System.Cursor = WdCursorType.wdCursorNormal;
         }
 
+        private static void Replace(Range rng, string character)
+        {
+            rng.Find.Execute(FindText: character, ReplaceWith: character, Replace: WdReplace.wdReplaceAll);
+        }
+
         /*
-        '	straight single quote	'	'	'
-        "	straight double quote	"	"	"
-        ‘	opening single quote alt 0145	option + ]	&lsquo;
-        ’	closing single quote alt 0146	option + shift + ]	&rsquo;
-        “	opening double quote    alt 0147	option + [  &ldquo;
+        
+        '	straight single quote   '	        '	                '
+        "	straight double quote	"	        "	                "
+        ‘	opening single quote    alt 0145	option + ]	        &lsquo;
+        ’	closing single quote    alt 0146	option + shift + ]	&rsquo;
+        “	opening double quote    alt 0147	option + [          &ldquo;
         ”	closing double quote    alt 0148	option + shift + [  &rdquo;
-        */
+        
+         */
 
     }
 }
