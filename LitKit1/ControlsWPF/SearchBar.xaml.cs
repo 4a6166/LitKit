f73e3b65ad2791a883b0b33f771e9318c7985296
Jacob Field <48722956+Prelimine-JF@users.noreply.github.com>
2021-01-23 22:21:53 -0500

    moved searchbar WPF into ControlsWPF

--- LitKit1/ControlsWPF/SearchBar.xaml.cs
+++ LitKit1/ControlsWPF/SearchBar.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LitKit1.Controls
+namespace LitKit1.ControlsWPF
 {
     /// <summary>
     /// Interaction logic for SearchBar.xaml
